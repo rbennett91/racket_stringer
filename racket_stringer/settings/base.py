e17fce66b134f6205ab688ab730a8ed63738b1a4
ryan bennett <ryan.bennett@outlook.com>
2020-06-27 17:05:07 -0400

    change static url

--- racket_stringer/settings/base.py
+++ racket_stringer/settings/base.py
@@ -90,7 +90,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = '/var/www/racket_stringer/static/'
-STATIC_URL = '/static/'
+STATIC_URL = '/racket_stringer/static/'
 
 # django-crispy-forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
