90a74bff7c551e607218a96a1d36eb135624835f
Ryan Bennett <>
2023-10-14 15:01:40 -0400

    update django, python pkgs, static files

--- racket_stringer/settings/production.py
+++ racket_stringer/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
