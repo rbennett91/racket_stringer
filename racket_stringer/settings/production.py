1e1753367ce117104a552f3552ca9497839f9e46
Ryan Bennett <>
2023-10-14 15:44:18 -0400

    swap database from postgres to sqlite3 :(

--- racket_stringer/settings/production.py
+++ racket_stringer/settings/production.py
@@ -13,12 +13,8 @@ ALLOWED_HOSTS = [os.environ["ALLOWED_HOSTS"]]
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["DB_NAME"],
-        "USER": os.environ["DB_USER"],
-        "PASSWORD": os.environ["DB_PASSWORD"],
-        "HOST": os.environ["DB_HOST"],
-        "PORT": os.environ["DB_PORT"],
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "racket_stringer.sqlite3",
     },
 }
 
